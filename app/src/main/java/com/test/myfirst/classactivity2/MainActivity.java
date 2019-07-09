ca1802bbdcbd41f8bc8db76b0371b8f2684786f8
Narendra Kumar <narendramuppuri@gmail.com>
2019-07-09 14:14:54 -0400

    Added Splash

--- app/src/main/java/com/test/myfirst/classactivity2/MainActivity.java
+++ app/src/main/java/com/test/myfirst/classactivity2/MainActivity.java
@@ -2,12 +2,29 @@ package com.test.myfirst.classactivity2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Intent;
+
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,ListActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
+
     }
 }
